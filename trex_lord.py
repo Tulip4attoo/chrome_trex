787b3bb628dd0a339477bc4869e47a34ae7b689b
tulip4attoo <huylinh.nguyen@example.com>
2017-10-27 16:35:25 +0700

    update GA model

---
+++ trex_lord.py
@@ -0,0 +1,225 @@
+import numpy as np
+import trex_nn
+import capturing_objects
+import time
+import logging
+
+
+###### PART 1: PREPARE FIXED VARs AND FUNCTIONs ######
+######################################################
+######################################################
+
+N_X = 3
+N_H = 3
+N_Y = 1
+
+POP_SIZE = 12
+MUTATION_PROB = 0.1
+N_SIZE = 4
+RANDOM_SET = [6, 5, 4, 3]
+BODY_KEYS = ["W1", "W2", "b1", "b2"]
+MUTATION_RANGE = [0.005, 0.5, 0.2, 0.05]
+
+def cv_to_sequence(body):
+    """
+    cv body aka parameters_set to ADN aka sequence
+    """
+    sequence = []
+    for key in BODY_KEYS:
+        sequence.append(body[key])
+    sequence_str = str(sequence)
+    sequence_str = sequence_str.replace("[", "").replace("array", "")
+    sequence_str = sequence_str.replace("]", "").replace("\n", "")
+    sequence_str = sequence_str.replace("(", "").replace(")", "")
+    sequence_str = sequence_str.replace(" ", "")
+    sequence_adj = map(float, sequence_str.split(","))
+    return sequence_adj
+
+
+def cv_to_body(adn):
+    """
+    cv ADN aka sequence to body aka parameters_set
+    tam thoi hard code 1 ty chu ko chac chet luon =))
+    """
+    params = {}
+    params["W1"] = np.reshape(adn[:9], (3, 3))
+    params["W2"] = np.reshape(adn[9:12], (1, 3))
+    params["b1"] = np.reshape(adn[12:15], (3, 1))
+    params["b2"] = np.reshape(adn[15], (1, 1))
+    return params
+
+
+def genesis(pop_size = POP_SIZE):
+    trex_clan = [trex_nn.initialize_parameters(N_X, N_H, N_Y) for i in xrange(POP_SIZE)]
+    trex_clan = np.array(trex_clan)
+    return trex_clan
+
+
+def random_match(random_set = RANDOM_SET):
+    number = -1
+    curr_number = sum(RANDOM_SET)
+    lucky_number = np.random.randint(0, curr_number)
+    while lucky_number < curr_number:
+        number += 1
+        curr_number -= RANDOM_SET[number]
+    return number
+
+
+def do_mutation(child, mutation_prob = MUTATION_PROB):
+    mutation_rate = np.random.random(16)
+    new_child = child[:]
+    for ind in xrange(16):
+        if mutation_rate[ind] < mutation_prob:
+            if ind < 9:
+                new_child[ind] += np.random.randn() * MUTATION_RANGE[0]
+            elif ind < 12:
+                new_child[ind] += np.random.randn() * MUTATION_RANGE[1]
+            elif ind < 15:
+                new_child[ind] += np.random.randn() * MUTATION_RANGE[2]
+            else:
+                new_child[ind] += np.random.randn() * MUTATION_RANGE[3]
+    return new_child
+
+
+def crossver(adam, eva):
+    weight_adam = adam[:12]
+    weight_eva = eva[:12]
+    bias_adam = adam[12:]
+    bias_eva = eva[12:]
+
+    cut_1 = np.random.randint(0, 12)
+    cut_2 = np.random.randint(12, 16) - 12
+
+    childs = []
+    childs.append(weight_adam[:cut_1] + weight_eva[cut_1:] + bias_adam[:cut_2] + bias_eva[cut_2:])
+    childs.append(weight_adam[:cut_1] + weight_eva[cut_1:] + bias_eva[:cut_2] + bias_adam[cut_2:])
+    childs.append(weight_eva[:cut_1] + weight_adam[cut_1:] + bias_adam[:cut_2] + bias_eva[cut_2:])
+    childs.append(weight_eva[:cut_1] + weight_adam[cut_1:] + bias_eva[:cut_2] + bias_adam[cut_2:])
+    selected_child = np.random.randint(0, 4)
+    return childs[selected_child]
+
+
+def breed_a_child(survivals):
+    """
+    chon 2 cha me tu survivals va crossver
+    sau do cho child mutation
+    """
+    adam_ind = random_match(RANDOM_SET)
+    eva_ind = random_match(RANDOM_SET)
+    # boi vi so ok kha it --> co the cu de tu breed cung dc di
+    # while adam_ind == eva_ind:
+    #     eva_ind = random_match(RANDOM_SET)
+    expected_cain = crossver(survivals[adam_ind], survivals[eva_ind])
+    real_cain = do_mutation(expected_cain)
+    return real_cain
+
+
+def gen_to_max_size(survivals, pop_size = POP_SIZE):
+    """
+
+    """
+    curr_len = N_SIZE
+    dna_survivals = map(cv_to_sequence, survivals)
+    dna_tribal = dna_survivals[:]
+    while curr_len < pop_size:
+        new_born = breed_a_child(dna_survivals)
+        dna_tribal.append(new_born)
+        curr_len += 1
+    new_gen = map(cv_to_body, dna_tribal)
+    new_gen = np.array(new_gen)
+    return new_gen
+
+
+def fitness_a_tribal_test(tribal):
+    """
+    get the fitness of a tribal
+    """
+    scores = np.random.random_sample(12)
+    return scores
+
+
+def select_survivals_test(tribal):
+    fitness_scores = fitness_a_tribal_test(tribal)
+    survival_inds = (-fitness_scores).argsort()[:4]
+    return tribal[survival_inds]
+
+
+def select_survivals(tribal, score):
+    fitness_scores = np.array(score)
+    survival_inds = (-fitness_scores).argsort()[:4]
+    return tribal[survival_inds], survival_inds
+
+
+
+######  PART 2: EVOLVE                          ######
+######################################################
+######################################################
+
+def evolve_test():
+    count = 0
+    adam_eva = genesis(POP_SIZE)
+    curr_gen = adam_eva.copy()
+    while count < 10:
+        survivals = select_survivals_test(curr_gen)
+        curr_gen = gen_to_max_size(survivals, POP_SIZE)
+        count += 1
+
+
+def evolve():
+    adam_eva = genesis(POP_SIZE)
+    curr_gen = adam_eva.copy()
+    count_gen = 0
+    while True:
+        log1.info("-------------------------------------------------------")
+        log2.info("-------------------------------------------------------")
+        print("-------------------------------------------------------")
+        log1.info("Generation: {}".format(count_gen))
+        log1.info("a new day has come")
+        score = []
+        for trex in curr_gen:
+            count_cactus = capturing_objects.play_game(trex)
+            score.append(count_cactus)
+        log1.info(score)
+        survivals, survival_inds = select_survivals(curr_gen, score)
+        log1.info(survival_inds)
+        log1.info("genarating next gen")
+        curr_gen = gen_to_max_size(survivals, POP_SIZE)
+        count_gen += 1
+        log2.info(survivals)
+        if max(score) > 15:
+            log1.info(survivals)
+            continue
+        elif count_gen % 10 == 0:
+            log1.info(survivals)
+        log1.info("")
+
+def setup_logger(logger_name, log_file, level=logging.INFO):
+    l = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(asctime)s : %(message)s')
+    fileHandler = logging.FileHandler(log_file, mode='w')
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    l.setLevel(level)
+    l.addHandler(fileHandler)
+    l.addHandler(streamHandler)    
+
+
+log_file_name_1 = time.strftime("./logs/%Y-%m-%d_%H:%M:%S_brief.log")
+log_file_name_2 = time.strftime("./logs/%Y-%m-%d_%H:%M:%S_survival.log")
+
+setup_logger('log1', log_file_name_1)
+setup_logger('log2', log_file_name_2)
+
+log1 = logging.getLogger('log1')
+log2 = logging.getLogger('log2')
+
+logging.basicConfig(filename=log_file_name_1, level=logging.INFO, 
+                    format="%(asctime)s %(levelname)s %(message)s")
+logging.basicConfig(filename=log_file_name_2, level=logging.INFO, 
+                    format="%(asctime)s %(levelname)s %(message)s")
+log1.info("Start")
+log2.info("Start")
+
+evolve()
---
+++ trex_nn.py
@@ -0,0 +1,111 @@
+import numpy as np
+import chrome_trex_api
+
+
+###### PART 1: PREPARE FIXED VARs AND FUNCTIONs ######
+######################################################
+######################################################
+
+
+def relu(array):
+    return np.maximum(array, 0.)
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+    
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+    
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.random.randn(n_h, 1) * 0.5
+    W2 = np.random.randn(n_y, n_h) * 1.5
+    b2 = np.random.randn(n_y, 1) * 0.15
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+
+def re_shape_X(X, n_x):
+    return np.reshape(X, (n_x, 1))
+
+######  PART 2: CREATE A NEURAL NETWORK MODEL   ######
+######################################################
+######################################################
+
+def tRex_model(X, parameters):
+    """
+    Argument:
+    X -- input data of size (n_x, m)
+    parameters -- python dictionary containing your parameters (output of initialization function)
+    
+    Returns:
+    A2 -- The sigmoid output of the second activation
+    """
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = sigmoid(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    return A2
+
+def from_model_to_action(value, threshold = 0.6):
+    if value > threshold:
+        chrome_trex_api.press_up()
+
+
+def wrap_model(X, parameters, n_x):
+    X_adj = re_shape_X(X, n_x)
+    action_value = tRex_model(X_adj, parameters).item()
+    # print(action_value)
+    from_model_to_action(action_value, threshold = 0.6)
+
+
+def test_help(X, parameters, n_x):
+    X_adj = re_shape_X(X, n_x)
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+
+    Z1 = np.dot(W1, X_adj) + b1
+    A1 = sigmoid(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    print(Z1)
+    print(A1)
+    print(Z2)
+    print(A2)
+
+######      PART 3: RUN AND LOAD THE MODEL      ######
+######################################################
+######################################################
